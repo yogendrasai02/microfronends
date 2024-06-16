8edc345708d805aba64290e9b23116c8136ff665
Yogendra Sai <yogendrasai02@gmail.com>
2024-06-16 19:12:35 +0530

    Updated AWS default region

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 
 export default function App() {
-  console.log('Container!');
+  console.log('Container, testing to see Github Action - AWS Infra integration');
   return (
     <>
       <h1>Container App!</h1>
