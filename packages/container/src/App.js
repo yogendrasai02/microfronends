7d51bb7165167414bb7632117d5ba335a78b64c3
Yogendra Sai <yogendrasai02@gmail.com>
2024-06-16 21:34:50 +0530

    Added cache invalidation command for index.html

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,10 +3,9 @@ import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 
 export default function App() {
-  console.log('Container, testing to see Github Action - AWS Infra integration');
   return (
     <>
-      <h1>Container App!</h1>
+      <h1>Container App!!!!</h1>
       <hr />
       <MarketingApp />
     </>
