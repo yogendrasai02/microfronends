ff647c53fb854fabf8ff322e1c83437662000484
Yogendra Sai <yogendrasai02@gmail.com>
2024-06-16 16:47:54 +0530

    Created container's deploy scripts

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,7 @@ import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 
 export default function App() {
+  console.log('Container!');
   return (
     <>
       <h1>Container App!</h1>
